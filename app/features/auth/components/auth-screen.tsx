f979b27a891b73c20a342026c001ec91cb28209c
alissonnxavier <alissonalves29@gmail.com>
2025-01-13 13:18:56 -0300

    added link in auth page to redirect user from home page

--- app/features/auth/components/auth-screen.tsx
+++ app/features/auth/components/auth-screen.tsx
@@ -5,6 +5,7 @@ import { SignInFlow } from "../types";
 import SignInCard from "./sign-in-card";
 import SignUpCard from "./sign-up-card";
 import Image from "next/image";
+import Link from "next/link";
 
 
 const AuthScreen = () => {
@@ -15,15 +16,18 @@ const AuthScreen = () => {
     <div className="w-full flex  justify-center bg-[#0a021c]">
       <div className="md:h-auto md:w-[420px]">
         <div className="flex justify-center mt-10 mb-8 ">
-          <div className="shine-border p-1 rounded-lg ">
-            <Image
-              className="rounded-lg"
-              width={200}
-              height={200}
-              alt="team-logo"
-              src="/barbaro.jpg"
-            />
-          </div>
+          <Link href='/'>
+            <div className="shine-border p-1 rounded-lg ">
+              <Image
+                className="rounded-lg"
+                width={200}
+                height={200}
+                alt="team-logo"
+                src="/barbaro.jpg"
+                priority
+              />
+            </div>
+          </Link>
         </div>
         <div>
           {state === "signIn" ? <SignInCard setState={setState} /> : <SignUpCard setState={setState} />}
