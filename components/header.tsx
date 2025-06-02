db7d180a0e5d2abb90efa9e093233b8b264d501f
alissonnxavier <alissonalves29@gmail.com>
2025-06-02 02:49:38 -0300

    fixed sm layout

--- components/header.tsx
+++ components/header.tsx
@@ -8,7 +8,7 @@ interface HeaderProps {
 
 export const Header = ({ clanData }: HeaderProps) => {
     return (
-        <div className="flex flex-col m-5 ml-10 h-72 xl:w-full">
+        <div className="flex flex-col m-5 ml-10 h-48 xl:w-full">
             <div className="text-3xl font-bold">
                 {clanData.data.clanData.name}
             </div>
@@ -25,26 +25,12 @@ export const Header = ({ clanData }: HeaderProps) => {
                 Pais {clanData.data.clanData.location.name}
             </div>
             <div className="text-muted-foreground text-xs">
-                Clan lever {clanData.data.clanData.clanLevel}
-            </div>
-            <div className="text-muted-foreground text-xs">
-                Pontos do clan {clanData.data.clanData.clanPoints}
-            </div>
-            <div className="text-muted-foreground text-xs">
                 Frequencia de guerra {clanData.data.clanData.warFrequency}
-            </div>
-            <div className="text-muted-foreground text-xs">
-                Guerras ganhas {clanData.data.clanData.warWins}
-            </div>
-            <div className="text-muted-foreground text-xs">
-                Guerras empatadas {clanData.data.clanData.warTies}
             </div>
             <div className="text-muted-foreground text-xs">
                 Liga {clanData.data.clanData.warLeague.name}
             </div>
-            <div className="text-muted-foreground text-xs">
-                Membros {clanData.data.clanData.members}
-            </div>
+
         </div>
     )
 }
